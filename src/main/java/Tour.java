7bac03fe5953fa301c740c99a2f75fb1d18d4165
AnoJo <benjamin.duchanois@etud.u-picardie.fr>
2021-11-24 04:48:58 +0100

    Creation des pièces et du placement initial

---
+++ src/main/java/Tour.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Tour extends Piece{
+			public Tour(int x, int y)
+			{
+				super(x, y);
+				this.nom = "Tour";
+				File input;
+				try
+				{
+					if(this.blanc) input = new File("images/Tour_Blanc.png");
+					else input = new File("images/Tour_Noir.png");
+					this.img = ImageIO.read(input);
+				} 
+		
+				catch(IOException e) 
+				{
+					e.printStackTrace();
+				}
+		
+			}
+}
